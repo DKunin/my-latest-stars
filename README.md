4b0d387c86a31fb84df14d739806044673080323
Dmitry Kunin <dkunin@avito.ru>
2016-02-25 15:43:54 +0300

    Added demogif, -u option to input github-username

--- README.md
+++ README.md
@@ -12,8 +12,14 @@ There must be global GIT_HUB_USER variable set to your github username.
           -h, --help   Show help
           -l, --limit  Limit of the stars to show, default is 10. 0 = no
                        limit.
+          -u, --user   If you don't want to make global username variable, you can just
+               pass in your github username as argument
 
         Made by DKunin http://dkunin.github.io/
+
+## In use
+
+![gif](./media/my-latest-stars.gif)
 
 ## License
  
--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 var request = require('superagent');
 var elegantStatus = require('elegant-status');
 var mainRequest = elegantStatus('Gazing at the stars');
-var gitHubName = process.env.GIT_HUB_USER;
 var argv = require('yargs')
     .usage('Usage: my-latest-stars [options]')
     .help('h')
@@ -11,10 +10,14 @@ var argv = require('yargs')
     .describe('l', 'Limit of the stars to show, default is 10. 0 = no limit.')
     .alias('l', 'limit')
     .default('l', 10)
+    .describe('u', 'If you don\'t want to make global username variable, you can just pass in your github username as argument')
+    .alias('u', 'user')
     .epilogue('Made by DKunin http://dkunin.github.io/').argv;
 
+var gitHubName = argv.u || process.env.GIT_HUB_USER;
+
 if (!gitHubName) {
-    throw new Error('GIT_HUB_USER global variable must be defined');
+    throw new Error('GIT_HUB_USER global variable must be defined, or you can pass it as -u option');
 }
 
 request('https://api.github.com/users/' + gitHubName + '/starred').end(function(err, data) {
